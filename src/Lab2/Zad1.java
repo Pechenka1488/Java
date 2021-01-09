bbc5744d1b3ef8c822a1877c1f1a024e685715ba
mefod <mefod@DESKTOP-SQAPS6J>
2021-01-09 13:56:28 +0700

    Comment

---
+++ src/Lab2/Zad1.java
@@ -0,0 +1,15 @@
+/*
+Вывести на консоль следующий набор символов:
+а=1, c=a+1 m=2, n=3 решение уравнений
+b=6, d=b*2-1 p=4, g=5 нахождение корней
+ */
+package Lab2;
+/**
+ *
+ * @author user
+ */
+public class Zad1 {
+    public static void main(String[] args){
+        System.out.println("а=1, c=a+1 m=2, n=3 решение уравнений\nb=6, d=b*2-1 p=4, g=5 нахождение корней");
+    }
+}
